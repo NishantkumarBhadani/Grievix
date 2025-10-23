1d6f5ccae7b99af5b262b0762fb3d3b2d29db4cd
Nishant Kumar Bhadani <134142263+NishantkumarBhadani@users.noreply.github.com>
2025-10-23 23:00:34 +0530

    refactor: improve server startup code and logging

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,22 +1,22 @@
 import dotenv from 'dotenv';
 import { app } from './app.js';
-import sequelize, {connectDB} from './db/index.js';
+import sequelize, { connectDB } from './db/index.js';
 
 dotenv.config({
-    path:'./.env'
+  path: './.env',
 });
 
-// console.log("JWT Secret at startup: ",process.env.JWT_ACCESS_SECRET)
-
 connectDB()
-.then(async ()=>{
-    await sequelize.sync(); 
-    app.listen(process.env.PORT || 3000, ()=>{
-        console.log(`Server is running at port :${process.env.PORT}`);
-        
-    })
-})
-.catch((err)=>{
-    console.log("DB connection failed");
-    
-})
+  .then(async () => {
+    await sequelize.sync();
+
+    const PORT = process.env.PORT || 3000;
+    const HOST = '0.0.0.0';
+
+    app.listen(PORT, HOST, () => {
+      console.log(`Server is running on http://${HOST}:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB connection failed:", err.message);
+  });
